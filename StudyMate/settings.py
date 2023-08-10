cef8279171a4f088aab59e84be751370f45e0a12
r3troseer <piagbo3d@gmail.com>
2023-08-10 22:58:27 +0100

    updated db

--- StudyMate/settings.py
+++ StudyMate/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = "static/"
 
 
 STATIC_ROOT = Path(BASE_DIR).joinpath("staticfiles")
-STATICFILES_DIRS = Path(BASE_DIR).joinpath("static")
+# STATICFILES_DIRS = Path(BASE_DIR).joinpath("static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
