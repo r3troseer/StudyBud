27eb5d162edb7cc12610f42b0c344c709a82cc61
r3troseer <piagbo3d@gmail.com>
2023-08-11 00:13:47 +0100

    moved broker url to env

--- StudyMate/settings.py
+++ StudyMate/settings.py
@@ -146,5 +146,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = config('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND')
