0697b8df15e9ca68c1095377667f0b037b374a81
r3troseer <piagbo3d@gmail.com>
2023-08-11 09:03:47 +0100

    updated celery

--- StudyMate/celery.py
+++ StudyMate/celery.py
@@ -1,7 +1,12 @@
 import os
 from celery import Celery
+from ssl import CERT_NONE
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StudyMate.settings")
-app = Celery("StudyMate")
+app = Celery(
+    "StudyMate",
+    broker_use_ssl={"ssl_cert_reqs": CERT_NONE},
+    redis_backend_use_ssl={"ssl_cert_reqs": CERT_NONE},
+)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
